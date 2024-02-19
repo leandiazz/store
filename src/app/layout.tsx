d003f0de0f6972e42259b9d8129280cedefef248
Leandro Diaz <leandrodiaz042@gmail.com>
2024-02-19 16:34:21 -0300

    add footer, work in home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,79 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 
 import "./globals.css";
 import Link from "next/link";
+import NavBar from "@/(components)/NavBar";
 
 export const metadata: Metadata = {
   title: "Cruel Summer",
   description: "Tienda de ropa",
-  keywords: ["tops", "vestidos", "remerones", "pantalones"]
+  keywords: ["tops", "vestidos", "remerones", "pantalones", "faldas"]
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto]">
+      <body className="m-auto grid min-h-screen grid-rows-[auto,1fr,auto]">
         {/* <h2 className="text-center bg-purple-400 text-black">
           Bienvenidx a nuestra tienda online!
         </h2> */}
-        <header className="text-xl font-bold leading-[3rem]">
-          <Link href="/">Cruel Summer</Link>
-        </header>
+        <NavBar />
         <main className="py-8">{children}</main>
-        <footer className="text-center leading-[3rem] opacity-70">
-          © {new Date().getFullYear()} Cruel Summer
+        <footer>
+          <div className="border-t-[1px] mx-10">
+            <h5 className="mt-5 mb-2 text-xl">Medios de pago</h5>
+            <div className="flex justify-start">
+              <img
+                src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/deposito.png"
+                alt="medio de pago"
+                className="h-[20px] w-auto my-0 mx-[5px]"
+              />
+              <img
+                src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/efectivo.png"
+                alt="medio de pago"
+                className="h-[20px] w-auto my-0 mx-[5px]"
+              />
+              <img
+                src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/acordar.png"
+                alt="medio de pago"
+                className="h-[20px] w-auto my-0 mx-[5px]"
+              />
+            </div>
+            <h5 className="mt-5 mb-2 text-xl">Medios de envío</h5>
+            <div className="flex justify-start">
+              <img
+                src="https://dk0k1i3js6c49.cloudfront.net/iconos-envio/correo-argentino.png"
+                alt="medio de envío"
+                className="h-[20px] w-auto my-0 mx-[5px]"
+              />
+              <img
+                src="https://dk0k1i3js6c49.cloudfront.net/iconos-envio/retiro-local.png"
+                alt="medio de envío"
+                className="h-[20px] w-auto my-0 mx-[5px]"
+              />
+              <img
+                src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/acordar.png"
+                alt="medio de envío"
+                className="h-[20px] w-auto my-0 mx-[5px]"
+              />
+            </div>
+            <h5 className="mt-5 mb-2 text-xl">Nuestras redes sociales</h5>
+            <Link href="https://facebook.com/Cruelsummerind">facebook</Link>
+            <br />
+            <Link href="https://instagram.com/cruelsummer.ind">instagram</Link>
+            <h5 className="mt-5 mb-2 text-xl">Contacto</h5>
+            <a href="mailto:cruelsummer.ind@gmail.com" target="_blank">
+              cruelsummer.ind@gmail.com
+            </a>
+            <br />
+            <a href="sms:1134060366" target="_blank">
+              1134060366
+            </a>
+          </div>
+          <div className="text-center leading-[3rem] opacity-70">
+            © {new Date().getFullYear()} Cruel Summer
+          </div>
         </footer>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,56 @@
-export default function Home() {
-  return <h1>Home Page</h1>;
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+  const targets = [
+    {
+      id: 1,
+      title: "tops",
+      src: "/tops.jpeg"
+    },
+    {
+      id: 2,
+      title: "faldas",
+      src: "/tops.jpeg"
+    },
+    {
+      id: 3,
+      title: "pantalones",
+      src: "/tops.jpeg"
+    },
+    {
+      id: 4,
+      title: "remerones",
+      src: "/remerones.jpeg"
+    },
+    {
+      id: 5,
+      title: "vestidos",
+      src: "/vestidos.jpeg"
+    }
+  ];
+
+  return (
+    <>
+      <Image src="/1.jpeg" width={1920} height={1080} alt="1" />
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 mt-10 p-10">
+        {targets.map(target => {
+          return (
+            <article key={target.id}>
+              <Link href={``}>
+                <img
+                  alt={target.title}
+                  className="mb-3 h-[300px] w-full object-cover"
+                  src={target.src}
+                />
+                <h2 className="text-3xl text-center font-bold">
+                  <p>{target.title}</p>
+                </h2>
+              </Link>
+            </article>
+          );
+        })}
+      </section>
+    </>
+  );
 }
