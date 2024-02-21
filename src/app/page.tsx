6079c9925d5dde00d7946d9caecdf37b24a4685d
Leandro Diaz <leandrodiaz042@gmail.com>
2024-02-21 15:51:01 -0300

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,12 @@
 import Link from "next/link";
 import { HomePictures } from "./components/HomePictures";
+import ProductTypesSlider from "./components/ProductTypesSlider";
 
 export default async function Home() {
-  const targets = [
-    {
-      id: 1,
-      title: "tops",
-      src: "/tops.jpeg"
-    },
-    {
-      id: 2,
-      title: "faldas",
-      src: "/tops.jpeg"
-    },
-    {
-      id: 3,
-      title: "pantalones",
-      src: "/tops.jpeg"
-    },
-    {
-      id: 4,
-      title: "remerones",
-      src: "/remerones.jpeg"
-    },
-    {
-      id: 5,
-      title: "vestidos",
-      src: "/vestidos.jpeg"
-    }
-  ];
-
   return (
     <>
       <HomePictures />
-      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 mt-10 p-10">
-        {targets.map(target => {
-          return (
-            <article key={target.id}>
-              <Link href={``}>
-                <img
-                  alt={target.title}
-                  className="mb-3 h-[300px] w-full object-cover"
-                  src={target.src}
-                />
-                <h2 className="text-3xl text-center font-bold">
-                  <p>{target.title}</p>
-                </h2>
-              </Link>
-            </article>
-          );
-        })}
-      </section>
+      <ProductTypesSlider />
     </>
   );
 }
