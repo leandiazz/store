e50e42cc066e3dbc50c09fe3fd71ea281bcc6b8a
Leandro Diaz <leandrodiaz042@gmail.com>
2024-02-22 21:24:08 -0300

    update layout

---
+++ src/app/error.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+async function error() {
+  return <p>ocurrio un error, intentalo de nuevo</p>;
+}
+export default error;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,17 +10,43 @@ const raleway = Raleway({ subsets: ["latin"], weight: ["500"] });
 
 export const metadata: Metadata = {
   title: "Cruel Summer",
-  description: "Tienda de ropa",
-  keywords: ["tops", "vestidos", "remerones", "pantalones", "faldas"]
+  description:
+    "Descubre la última moda en nuestra tienda de ropa femenina. Ofrecemos una amplia gama de faldas, tops, pantalones y remerones de alta calidad y estilo. ¡Viste con confianza y estilo con nuestras prendas únicas!",
+  keywords: ["tops", "vestidos", "remerones", "pantalones", "faldas", "ropa"],
+  category: "indumentaria",
+  openGraph: {
+    title: "Cruel Summer",
+    description: "Tienda de ropa femenina, prendas únicas!",
+    url: "https://cruelsummer.vercel.app",
+    siteName: "Cruel Summer",
+    images: [
+      {
+        url: "https://cruelsummer.vercel.app/PIC.png",
+        width: 560,
+        height: 365,
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn("relative h-full font-sans antialiased", raleway.className)}>
+      <body
+        className={cn(
+          "relative h-full font-sans antialiased",
+          raleway.className,
+        )}
+      >
         <Navbar />
-        <main className="relative flex flex-col min-h-screen">
-          <div className="flex-grow flex-1">{children}</div>
+        <main className="relative flex min-h-screen flex-col">
+          <div className="flex-1 flex-grow">{children}</div>
         </main>
         <Footer />
       </body>
