062e2e778285835d57aeafbde22fc2db12d02931
Leandro Diaz <leandrodiaz042@gmail.com>
2024-03-10 08:13:49 -0300

    update code

--- src/app/_components/FooterAccordion.tsx
+++ src/app/_components/FooterAccordion.tsx
@@ -11,35 +11,81 @@ export default function FooterAccordion() {
       <AccordionItem value="item-1">
         <AccordionTrigger>¿Como me puedo contactar?</AccordionTrigger>
         <AccordionContent>
-          <p>
-            Email:{" "}
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-mail mb-1 mr-2"
+              width={25}
+              height={25}
+              viewBox="0 0 24 24"
+              strokeWidth={1.25}
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+              <path d="M3 7l9 6l9 -6" />
+            </svg>
             <a href="cruelsummer.ind@gmail.com" target="_blank" className="hover:opacity-40" rel="">
-              cruelsummer.ind@gmail.com
+              <span> cruelsummer.ind@gmail.com</span>
             </a>
-          </p>
+          </div>
 
-          <p>
-            Instagram:{" "}
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-instagram mr-2"
+              width={26}
+              height={26}
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+              <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+              <path d="M16.5 7.5l0 .01" />
+            </svg>
             <a
               href="https://www.instagram.com/cruelsummer.ind/"
               target="_blank"
               className="hover:opacity-40"
               rel=""
             >
-              @cruelsummer.ind
+              <span>cruelsummer.ind</span>
             </a>
-          </p>
-          <p>
-            Whatsapp:{" "}
+          </div>
+          <div className="mt-1 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-whatsapp mr-2"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+              <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+            </svg>
             <a
               href="https://wa.me/5491134060366"
               target="_blank"
               className="hover:opacity-40"
               rel=""
             >
-              1134060366
+              <span>1134060366</span>
             </a>
-          </p>
+          </div>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
