2acdf34f2ec5361a2430bb99076177a3c9fa0034
Leandro Diaz <leandrodiaz042@gmail.com>
2024-03-15 16:26:04 -0300

    fix Cartlist products ScrollArea not growing in height

--- src/app/components/CartList.tsx
+++ src/app/components/CartList.tsx
@@ -1,4 +1,4 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea2 } from "@/components/ui/scroll-area";
 import { useCartData, useCart } from "@/hooks/useCart";
 import { Separator } from "@/components/ui/separator";
 import CartProductCard from "../carrito/CartProductCard";
@@ -8,16 +8,17 @@ export default function CartList() {
   const { items } = useCart();
 
   return (
-    <section>
-      <ScrollArea className="mr-4 h-[450px] pr-2 lg:h-[450px]">
-        <div className="flex h-full flex-col justify-between ">
+    <section className="h-full">
+      <ScrollArea2 className="mr-4 w-full pr-4">
+        <div className="flex h-full flex-col justify-between">
           <ul className="mt-5 list-none">
             {items.map((cartItem) => (
               <CartProductCard cartItem={cartItem} key={cartItem.key} />
             ))}
           </ul>
         </div>
-      </ScrollArea>
+      </ScrollArea2>
+
       <Separator className="my-2" />
       <footer>
         <p className="flex justify-between px-4">
