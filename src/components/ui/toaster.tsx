45670f4080b16c51d472e09f75e90f81440adc19
Leandro Diaz <leandrodiaz042@gmail.com>
2024-03-17 19:44:59 -0300

    fix action buttom to close to right

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,17 +1,18 @@
-"use client"
+"use client";
 
 import {
   Toast,
+  ToastAction,
   ToastClose,
   ToastDescription,
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
@@ -20,16 +21,16 @@ export function Toaster() {
           <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
+              {description && <ToastDescription>{description}</ToastDescription>}
             </div>
-            {action}
+            <ToastAction altText="Carrito" asChild>
+              {action}
+            </ToastAction>
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
